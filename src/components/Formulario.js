93258f27c4033e92bfc5c19c38dd5c856e19f1e5
carlosbotero22 <monoprograma22@gmail.com>
2023-03-09 12:52:51 -0500

    formulario con libreria

---
+++ src/components/Formulario.js
@@ -0,0 +1,40 @@
+// ***************************************
+// ******* 👨‍💻DEV_CARLOS BOTERO👨‍💻*********
+// ***************************************
+
+import React, { Fragment } from "react";
+import { useForm } from "react-hook-form";
+
+// con esta libreria React-Hook-Form validamos de forma sencilla
+
+function Formulario(props) {
+    const { register, handleSubmit, formState: { errors }} = useForm();
+
+    const onSubmit = (values,e) => {
+        console.log(values)
+        e.target.reset()
+    };
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1>React-Hook-Form</h1>
+          <input
+            className="form-control my-2"
+            name="message"
+            autoComplete="off"
+            {...register("message", {
+              required: "Titulo Obligatorio🚫",
+            })}
+          />
+
+          <span className="text-danger">
+                {errors.message && errors.message.message}
+          </span>
+
+          <input className="btn btn-primary" type="submit" />
+        </form>
+      </div>
+    );
+  }
+export default Formulario;
